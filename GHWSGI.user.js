91cca2adcd6c0b71542b6a83701106a926e6c763
Alexandre Dotor Casals <alex.dotor@gmail.com>
2018-09-02 14:38:45 +0200

    Added support for parsing Wikis in Markdown mode (url generation is still buggy as it may be not standard and there is no way to get it afaik). GHWSGI button is just a placeholder for now. It'll hold a config modal to define the mode between html/markdown License

--- GHWSGI.user.js
+++ GHWSGI.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github Wiki Steamgifts Integration
 // @namespace    GHWSGI
-// @version      1.0
+// @version      1.0.1
 // @description  Integrate Github wikis into Steamgift discussion
 // @author       knsys
 // @downloadURL  https://github.com/knsys/GHWSGI/raw/master/GHWSGI.user.js
@@ -11,56 +11,116 @@
 // @connect      github.com
 // @connect      githubusercontent.com
 // @resource     ghcss https://raw.githubusercontent.com/knsys/GHWSGI/master/ghwsgi.min.css
+// @require      https://cdn.rawgit.com/showdownjs/showdown/1.8.6/dist/showdown.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const wikiLinkPrefix = 'wiki-gh-';
+    const wikiLinkPrefix = 'wiki-gh';
     const wikiSectionClass = "wiki-gh-content";
     const githubBaseUrl = 'https://github.com';
-    addStyle();
-    fillWikiLinks();
+    const githubMarkdownBaseUrl = 'https://raw.githubusercontent.com';
+    const defaultMode = "html";
+    let wikiParser = undefined;
 
-    async function addStyle(){
-        const cssUrl = await GM.getResourceUrl('ghcss');
-        $('head').append(`<link rel="stylesheet" href="${cssUrl}" type="text/css" />`);
+    /**
+    * HTML Class
+    */
+    class HtmlWiki {
+        constructor() {
+            this.addStyle();
+        }
+
+        async addStyle(){
+            const cssUrl = await GM.getResourceUrl('ghcss');
+            $('head').append(`<link rel="stylesheet" href="${cssUrl}" type="text/css" />`);
+        }
+
+        getHtmlToDisplay(html){
+            var el = $( '<div></div>' );
+            el.html(html);
+            let wikiWrapper = $('#wiki-wrapper', el);
+            $('#wiki-rightbar', wikiWrapper).remove();
+            // Update relative links to redirect to github page in a new tab
+            let relativeLinks = $('a[href^="/"]', wikiWrapper);
+            relativeLinks.each(async function() {
+                $(this).attr('href', githubBaseUrl + $(this).attr('href'));
+                $(this).attr('target', '_BLANK');
+            });
+            return wikiWrapper.html();
+        }
+
+        generateUrlFromLink({user, repo, type, pageUrl}){
+            if (type !== 'wiki') return undefined;
+            return `${githubBaseUrl}/${user}/${repo}/${pageUrl}`;
+        }
     }
 
-    async function fillWikiLinks(){
+    /**
+    * Markdown Class
+    */
+     class MarkDownWiki {
+        constructor() {}
+
+        async addStyle(){
+            const cssUrl = await GM.getResourceUrl('ghcss');
+            $('head').append(`<link rel="stylesheet" href="${cssUrl}" type="text/css" />`);
+        }
+
+        getHtmlToDisplay(markdown){
+            const converter = new showdown.Converter();
+            return converter.makeHtml(markdown);
+        }
+
+        generateUrlFromLink({user, repo, type, pageUrl}){
+            return `${githubMarkdownBaseUrl}/${type}/${user}/${repo}/${pageUrl}.md`;
+        }
+    }
+
+    bootstrap();
+    function bootstrap(){
+        addNavMenuEntry();
+        const mode = getLocalStorageValue('mode', defaultMode);
+        wikiParser = (mode === 'html') ? new HtmlWiki() : new MarkDownWiki();
+        fillWikiLinks(wikiParser);
+    }
+
+    function addNavMenuEntry(){
+        const navMenuEntry = '<div class="nav__button-container"><a class="nav__button" href="#">GHWSGI</a></div>';
+        $('.nav__left-container').append(navMenuEntry);
+    }
+
+     async function fillWikiLinks(wikiParser){
         $(`a[href^="${wikiLinkPrefix}"]`).each(async function() {
-            await appendWikiFromLink($(this));
+            await appendWikiFromLink($(this), wikiParser);
         });
     }
 
-    async function appendWikiFromLink(link){
+    async function appendWikiFromLink(link, wikiParser){
         let target = $( `<section><div></div></section>` );
         target.addClass(wikiSectionClass);
-        let wikiHtml = await getRequest(generateRealUrlFromGitHub(link));
+        const linkParams = segmentLinkParameters(link);
+        const url = wikiParser.generateUrlFromLink(linkParams);
+        if (url === undefined) return;
+        let wikiResponse = await getRequest(url);
         let subDiv = $(target.children('div')[0]);
         subDiv.addClass('jumbotron');
-        subDiv.html(cleanWikiPage(wikiHtml));
+        const html = wikiParser.getHtmlToDisplay(wikiResponse);
+        subDiv.html(html);
         link.replaceWith(target);
     }
 
-    function generateRealUrlFromGitHub(link){
-        return link.attr('href').replace(wikiLinkPrefix, `${githubBaseUrl}/`);
+    function segmentLinkParameters(link){
+        const params = link.attr('href').split('/');
+        if (params.length === 3) params.push('Home');
+        const pageUrl =  (params.length > 4) ? params.slice(3).join('/') : params[4];
+        return {user: params[1], repo: params[2], type: params[3], pageUrl: pageUrl};
     }
 
-    function cleanWikiPage(html){
-        var el = $( '<div></div>' );
-        el.html(html);
-        let wikiWrapper = $('#wiki-wrapper', el);
-        $('#wiki-rightbar', wikiWrapper).remove();
-        // Update relative links to redirect to github page in a new tab
-        let relativeLinks = $('a[href^="/"]', wikiWrapper);
-        relativeLinks.each(async function() {
-            $(this).attr('href', githubBaseUrl + $(this).attr('href'));
-            $(this).attr('target', '_BLANK');
-        });
-        return wikiWrapper.html();
-    }
-
+    /**
+    * Requests utils functions
+    */
     function getRequest(url) {
         return new Promise(function (resolve, reject) {
             GM.xmlHttpRequest({
@@ -78,5 +138,16 @@
                 }
             });
         });
+    }
+
+    /**
+    * LocalStorage Management
+    */
+    function setLocalStorageValue(key, newValue) {
+        localStorage.setItem(`ghwsgi-${key}`, newValue);
+    }
+
+    function getLocalStorageValue(key, defaultValue = undefined) {
+        return localStorage.getItem(`ghwsgi-${key}`) || defaultValue;
     }
 })();
